a1ed05705067ef906c896e6154e84e2b5174a697
William Colvill <89409681+Colvill44@users.noreply.github.com>
2022-12-29 23:15:59 -0500

    Add files via upload

---
+++ VoteCounter/src/votecounterproject/VoteCounterProject.java
@@ -0,0 +1,15 @@
+package votecounterproject;
+
+/**
+ * @version Spring 2019
+ * @author clatulip
+ */
+public class VoteCounterProject {
+
+    public static void main(String[] args) {
+        ArrayListVoteCounter.runRandomElectionResults();
+
+        // ArrayListVoteCounter.runRandomElectionResults();
+    }
+
+}
